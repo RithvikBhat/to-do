81eb1d6570c8c1be4e3ddf2262834eb79b581b5e
Rithvik <code.rithvikkbhat@gmail.com>
2021-06-26 21:38:18 +0530

    c

--- src/NotFound.js
+++ src/NotFound.js
@@ -12,11 +12,3 @@ const NotFound = () => {
 export default NotFound;
 
 
-//finally, 
-// YEEEEESSSSSSS WORKING!!!!
-// TUTUTUDDOOOOOOOOOOOOOO, FINALLY REACT DONE! NOW HAVE TO DO A LOT OF PROJECTS!
-// YENO AYTHU, yes working if you type /blogs/ its searching for data in data.json. HUUU
-// ANYWAYS WE CAN USE THIS AND CREATE OUR TO DO APP I GUESS
-// SHALL I PUSH IT TO GITHUHB HOW SHALL I SEND TO YOU? CO AUTHORED ANTA IDE 
-// I WILL SCREEN SHARE SEE
-// yesss because react files use madidu naavu alva
